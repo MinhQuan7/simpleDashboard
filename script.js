c64807891f5a468d47022868abd4e9645aa564d6
Quan Le <quan.lm@eoh.io>
2024-10-24 17:37:17 +0700

    24/10/2024

--- script.js
+++ script.js
@@ -17,3 +17,32 @@
         // Cập nhật đồng hồ mỗi giây
         updateClock();
         setInterval(updateClock, 1000);
+
+
+
+
+
+const progressBar = document.getElementsByClassName('progress-bar')[0]
+setInterval(() => {
+  const computedStyle = getComputedStyle(progressBar)
+  const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+  progressBar.style.setProperty('--width', width + .1)
+}, 5)
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem =>{
+    var dots = elem.getAttribute('data-dots')
+    var marked = elem.getAttribute('data-percent');
+    var percent = Math.floor(dots*marked/100);
+    var rotate = 360/dots;
+    var points = "";
+    for (let i = 0; i < dots; i++) {
+         points += `<div class="points" style="--i: ${i}; --rot: ${rotate}deg"></div>`;
+    }
+    elem.innerHTML = points;
+
+    const pointsMarked = elem.querySelectorAll('.points');
+    for (let i = 0; i < percent; i++) {
+        pointsMarked[i].classList.add('marked')
+    }
+})
