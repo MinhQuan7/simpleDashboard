918cb900ec546354a7a1793c7c9816152a468de0
Quan Le <quan.lm@eoh.io>
2024-10-25 14:22:02 +0700

    Simple Dashboard : Don't have middle Column

--- script.js
+++ script.js
@@ -22,12 +22,6 @@
 
 
 
-const progressBar = document.getElementsByClassName('progress-bar')[0]
-setInterval(() => {
-  const computedStyle = getComputedStyle(progressBar)
-  const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
-  progressBar.style.setProperty('--width', width + .1)
-}, 5)
 
 const circles = document.querySelectorAll('.circle');
 circles.forEach(elem =>{
